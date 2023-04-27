550f0e00208c26fb61f1fea0515a3e0f9294c358
Yonle <yonle@lecturify.net>
2023-04-27 14:33:20 +0700

    dong
    Signed-off-by: Yonle <yonle@lecturify.net>

--- modtools/nuke.sh
+++ modtools/nuke.sh
@@ -6,8 +6,8 @@ echo "Remove: $1"
 
 while read filename; do
   echo "Removing attachments: $filename"
-  rm "__uploads/${i}"
-  rm "__uploads/${i}.webp"
+  rm "__uploads/${filename}"
+  rm "__uploads/${filename}.webp"
 done<<<$(sqlite3 database.db "select furl from '$1';")
 
 sqlite3 database.db "drop table '$1';"
