71e1c3e02f748bb0a4aadc0690aab290fd199ad6
Yonle <yonle@lecturify.net>
2023-06-28 10:36:28 +0700

    Fix migration script
    Signed-off-by: Yonle <yonle@lecturify.net>

--- migrate-from-toard.sh
+++ migrate-from-toard.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
 for threadID in $(sqlite3 database.db 'SELECT name FROM sqlite_schema;'); do
-  sqlite3 database.db "ALTER TABLE '${threadID}' ADD furl TEXT;"
+  if [ "$threadID" != "__threadlists" ]; then
+    sqlite3 database.db "ALTER TABLE '${threadID}' ADD furl TEXT;"
+  fi
 done
