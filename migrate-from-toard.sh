7dedd6a8fc9af342e45d88930607d5f57348bb5c
Yonle <yonle@lecturify.net>
2023-06-28 10:40:13 +0700

    migration script: read from __threadlists instead.
    Signed-off-by: Yonle <yonle@lecturify.net>

--- migrate-from-toard.sh
+++ migrate-from-toard.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-for threadID in $(sqlite3 database.db 'SELECT name FROM sqlite_schema;'); do
+for threadID in $(sqlite3 database.db 'SELECT id FROM __threadlists;'); do
   if [ "$threadID" != "__threadlists" ]; then
     sqlite3 database.db "ALTER TABLE '${threadID}' ADD furl TEXT;"
   fi
